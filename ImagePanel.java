fdcd2eba91dd4ca2d3c4a084b9ac5c716ab50432
Adam Howard <achdiposable1@gmail.com>
2017-08-16 12:10:33 +0100

    fixed indentation; favoured composition over inheritance with MouseListener and MouseMotionListener

--- ImagePanel.java
+++ ImagePanel.java
@@ -1,4 +1,6 @@
 import java.awt.Graphics;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseMotionAdapter;
 import java.awt.image.BufferedImage;
 import java.awt.event.MouseWheelListener;
 import java.awt.event.MouseMotionListener;
@@ -15,67 +17,64 @@ import java.awt.Graphics2D;
 //import java.awt.geom.AffineTransform;
 //import java.awt.image.AffineTransformOp;
 
-public class ImagePanel extends JComponent implements MouseListener, MouseWheelListener, MouseMotionListener
+public class ImagePanel extends JComponent implements MouseWheelListener
 {
-	private BufferedImage currentImage;
-	private double scale = 1;
-	private double zoomAmount = 1.1;
-
-	private double placementX;
-	private double placementY;
+    int bogus = Integer.MIN_VALUE;
+    private BufferedImage currentImage;
+    private double scale = 1;
+    private double zoomAmount = 1.1;
+    private double placementX;
+    private double placementY;
     private double drawHeight;
     private double drawWidth;
-	private int mouseX = 0;
-	private int mouseY = 0;
-	
-    int bogus = -999999;
-    
-	private int oldDragX = bogus;
-	private int oldDragY = bogus;
-	
-	private boolean firstPaint = true;
-	private String imageFilename = "";
-	
-	private boolean canMousePress = false;
-	private boolean mousePressed = false;
-	
-	private ImageTagger parent;
-	public ImagePanel(ImageTagger parent)
-	{
-		this.parent = parent;
-		addMouseWheelListener(this);
-		addMouseMotionListener(this);
-		addMouseListener(this);
-	}
-	
-	public void loadImage(File imageToLoad)
-	{
-		try
-		{
-			currentImage = ImageIO.read(imageToLoad);
-			parent.setTitle(imageToLoad.getName());//change title to filename
-			imageFilename = imageToLoad.getName();
-			//debugging info
-			parent.debugPrint("Image Width: "+currentImage.getWidth());
-			parent.debugPrint("Image Height: "+currentImage.getHeight());
-			double aspectRatio = (double)currentImage.getWidth() / (double)currentImage.getHeight();
-			//parent.debugPrint("Aspect Ratio: "+aspectRatio);
-			System.out.println("Aspect Ratio: "+aspectRatio);
-			firstPaint = true;
-			repaint();
-		}
-		//Very old catch blocks from image validator code.
-		//Could still come in handy...
-		catch(IOException e)
-		{System.err.println("IO Exception in \n" + imageToLoad);}
-		catch(IllegalArgumentException iaError)
-		{System.err.println("\n Illegal Argument Exception: " +
-			iaError.getMessage()+"\nin " + imageToLoad);}
-		catch(OutOfMemoryError memError)
-		{System.err.println("OutOfMemoryError: "+memError.getMessage()+
-			" on file "+imageToLoad+"; continuing ONWARDS!");}
-	}
-    
+    private int mouseX = 0;
+    private int mouseY = 0;
+    private int oldDragX = bogus;
+    private int oldDragY = bogus;
+
+    private boolean firstPaint = true;
+    private String imageFilename = "";
+
+    private boolean canMousePress = false;
+    private boolean mousePressed = false;
+
+    private ImageTagger parent;
+    public ImagePanel(ImageTagger parent)
+    {
+        this.parent = parent;
+        addMouseWheelListener(this);
+        addMouseMotionListener(mouseMotionAdapter);
+        addMouseListener(mouseAdapter);
+    }
+
+    public void loadImage(File imageToLoad)
+    {
+        try
+        {
+            currentImage = ImageIO.read(imageToLoad);
+            parent.setTitle(imageToLoad.getName());//change title to filename
+            imageFilename = imageToLoad.getName();
+            //debugging info
+            parent.debugPrint("Image Width: "+currentImage.getWidth());
+            parent.debugPrint("Image Height: "+currentImage.getHeight());
+            double aspectRatio = (double)currentImage.getWidth() / (double)currentImage.getHeight();
+            //parent.debugPrint("Aspect Ratio: "+aspectRatio);
+            System.out.println("Aspect Ratio: "+aspectRatio);
+            firstPaint = true;
+            repaint();
+        }
+        //Very old catch blocks from image validator code.
+        //Could still come in handy...
+        catch(IOException e)
+        {System.err.println("IO Exception in \n" + imageToLoad);}
+        catch(IllegalArgumentException iaError)
+        {System.err.println("\n Illegal Argument Exception: " +
+                iaError.getMessage()+"\nin " + imageToLoad);}
+        catch(OutOfMemoryError memError)
+        {System.err.println("OutOfMemoryError: "+memError.getMessage()+
+                " on file "+imageToLoad+"; continuing ONWARDS!");}
+    }
+
     public void fitImage()
     {
         //find out how much larger each dimension of the image is than the window;
@@ -91,10 +90,10 @@ public class ImagePanel extends JComponent implements MouseListener, MouseWheelL
             drawHeight = (double)currentImage.getHeight()/overWidth;
             placementX = 0;
             placementY = (getHeight()/2) - drawHeight/2;//centre the image's Y.
-            
+
             //double imgHeight = (double)currentImage.getHeight()/overWidth;
             //placementY = (getHeight()/2) - imgHeight/2;//centre the image's Y.
-            
+
             //g.drawImage(currentImage, 0, (int)placementY, getWidth(), (int)imgHeight, this);
             scale = 1.0 / overWidth;
         }
@@ -109,7 +108,7 @@ public class ImagePanel extends JComponent implements MouseListener, MouseWheelL
             scale = 1.0 / overHeight;
         }
     }
-    
+
     public void normalZoom()
     {
         //System.out.println("normal zoom!");
@@ -122,26 +121,26 @@ public class ImagePanel extends JComponent implements MouseListener, MouseWheelL
         //parent.stdPrint("current image width:"+placementWidth);
         //parent.stdPrint("current image height:"+placementHeight);
     }
-	
-	public void paint(Graphics g)
-	{
-		if(currentImage != null)//only do stuff if we have an image to draw -- prevents NullPointerExceptions
-		{
-			Graphics2D g2 = (Graphics2D)g;
-			//find out how much larger each dimension of the image is than the window;
-			
-			//before drawing the new image, clear the old one.
-			g.clearRect(0,0,getWidth(),getHeight());
+
+    public void paint(Graphics g)
+    {
+        if(currentImage != null)//only do stuff if we have an image to draw -- prevents NullPointerExceptions
+        {
+            Graphics2D g2 = (Graphics2D)g;
+//find out how much larger each dimension of the image is than the window;
+
+//before drawing the new image, clear the old one.
+            g.clearRect(0,0,getWidth(),getHeight());
             System.out.println("firstPaint:"+firstPaint);
-			if(firstPaint)//first time we draw a new image, make it fit the window
-			{
-                //find out how much larger each dimension of the image is than the window;
-				firstPaint = false;
+            if(firstPaint)//first time we draw a new image, make it fit the window
+            {
+//find out how much larger each dimension of the image is than the window;
+                firstPaint = false;
 
                 double overWidth = (double)currentImage.getWidth() / (double)getWidth();
                 double overHeight = (double)currentImage.getHeight() / (double)getHeight();
                 if(overWidth > 1.0
-                || overHeight > 1.0)
+                        || overHeight > 1.0)
                 {
                     fitImage();
                 }
@@ -150,149 +149,155 @@ public class ImagePanel extends JComponent implements MouseListener, MouseWheelL
                     normalZoom();
                 }
                 System.out.println("shaboieeee");
-			}
-			else
-			{//no longer first paint -- do zooming to mouse
-				drawWidth = (double)currentImage.getWidth() * scale;
-				drawHeight = (double)currentImage.getHeight() * scale;
+            }
+            else
+            {//no longer first paint -- do zooming to mouse
+                drawWidth = (double)currentImage.getWidth() * scale;
+                drawHeight = (double)currentImage.getHeight() * scale;
 
-				//g.drawImage(currentImage, (int)placementX, (int)placementY,
-				//(int)placementWidth, (int)placementHeight, this);
+//g.drawImage(currentImage, (int)placementX, (int)placementY,
+//(int)placementWidth, (int)placementHeight, this);
 
-				//parent.stdPrint("current image width:"+placementWidth);
-				//parent.stdPrint("current image height:"+placementHeight);
+//parent.stdPrint("current image width:"+placementWidth);
+//parent.stdPrint("current image height:"+placementHeight);
                 parent.stdPrint("zoom:"+(scale*100));
-			}
-            //draw the image with the inferred parameters
+            }
+//draw the image with the inferred parameters
             g.drawImage(currentImage, (int)placementX, (int)placementY, (int)drawWidth, (int)drawHeight, this);
-			
-			parent.setTitle(imageFilename+" -- "+((int)(scale*100))+"%");
-		}
-	}
-	
-	public void mouseMoved(MouseEvent me)
-	{}//empty
-	
-	public void	mouseClicked(MouseEvent me)
-	{}
-	public void	mouseEntered(MouseEvent me)
-	{
-		//System.out.println("enter");
-		canMousePress = true;
-	}
-	
-	public void	mouseExited(MouseEvent me)
-	{
-		//System.out.println("exit");
-		canMousePress = false;
-	}
-	
-	public void	mousePressed(MouseEvent me)
-	{
-		//System.out.println("press");
-		System.out.println("button pressed:"+me.getButton());
-		if(canMousePress)
-		{
-			mousePressed = true;
-			oldDragX = me.getX();
-			oldDragY = me.getY();
-		}
-	}
-	public void	mouseReleased(MouseEvent me)
-	{
-		//System.out.println("release");
-		if(canMousePress)
-			mousePressed = false;
-	}
-	
-	public void mouseDragged(MouseEvent me)
-	{
-		/*if(oldDragX == -99999
-		|| oldDragY == -99999)
-		{
-			oldDragX = me.getX();
-			oldDragY = me.getY();
-			return;
-		}*/
-		//System.out.println("dragging");
-		if(mousePressed)
-		{
-			if(firstPaint)
-			{
-				oldDragX = me.getX();
-				oldDragY = me.getY();
-			}
-			else
-			{
-				/*System.out.println("oldDragX,Y:"+oldDragX+","+oldDragY);
-				System.out.println("mouseX,Y:"+me.getX()+","+me.getY());
-				System.out.println("placementX,Y:"+placementX+","+placementY);*/
-				
-				int dragDiffX = me.getX() - oldDragX;
-				int dragDiffY = me.getY() - oldDragY;
-				
-				//System.out.println("dragDiffX,Y:"+dragDiffX+","+dragDiffY);
-				
-				placementX += dragDiffX;
-				placementY += dragDiffY;
-				
-				oldDragX = me.getX();
-				oldDragY = me.getY();
-				
-				repaint();
-			}
-		}
-	}
-	
-	public void mouseWheelMoved(MouseWheelEvent mwe)
-	{
-		//parent.stdPrint("MouseWheelEvent coords: "+mwe.getX()+","+mwe.getY());
-		mouseX = mwe.getX();
-		mouseY = mwe.getY();
 
-		//parent.stdPrint("placementX,Y:"+placementX+","+placementY);
-		
-		double pixelsLeft = mouseX - placementX;
-		double pixelsAbove = mouseY - placementY;
-		
-		//parent.stdPrint("pixelsLeft: "+pixelsLeft);
-		//parent.stdPrint("pixelsAbove: "+pixelsAbove);
-		
-		double oldScale = scale;
-		
-		double newPixelsAbove;
-		double newPixelsLeft;
-		//update the scale;
-		if(mwe.getWheelRotation() < 0)
-		{//zooming in
-			parent.stdPrint("zooming in");
-			scale *= zoomAmount;
-			repaint();
-			 newPixelsAbove = pixelsAbove * zoomAmount;
-			newPixelsLeft = pixelsLeft * zoomAmount;
-		}
-		else
-		{//zooming out
-			parent.stdPrint("zooming out");
-			scale /= zoomAmount;
-			repaint();
-			newPixelsAbove = pixelsAbove / zoomAmount;
-			newPixelsLeft = pixelsLeft / zoomAmount;
-		}
-		//parent.stdPrint("scale/oldScale="+(scale/oldScale));
-		//parent.stdPrint("oldScale/scale="+(oldScale/scale));
-		
-		//WELL BLOODY FRIGGING DONE TO ME FOR DERIVING A ZOOM ALGORITHM FROM SCRATCH ON MY OWN! :D
-		//todo: tidy up bad int and double conversions, avoid double arithmetic where possible
-			//-- to stop the zooming messing up towards the extremes
-		parent.stdPrint("scale change: "+oldScale+" to "+scale);
-		
-		//parent.stdPrint("new pixelsLeft: "+newPixelsLeft);
-		//parent.stdPrint("new pixelsAbove: "+newPixelsAbove);
-		
-		placementX += (pixelsLeft - newPixelsLeft);//YUSSS I HAVE FUCKING DONE IT AFTER 4 MONTHS
-		placementY += (pixelsAbove - newPixelsAbove);
-		
-		parent.stdPrint("new placementX,Y:"+placementX+","+placementY);		
-	}
+            parent.setTitle(imageFilename+" -- "+((int)(scale*100))+"%");
+        }
+    }
+
+    private MouseAdapter mouseAdapter = new MouseAdapter()
+    {
+        @Override
+        public void mouseEntered(MouseEvent me)
+        {
+//System.out.println("enter");
+            canMousePress = true;
+        }
+
+        @Override
+        public void mouseExited(MouseEvent me)
+        {
+//System.out.println("exit");
+            canMousePress = false;
+        }
+
+        @Override
+        public void mousePressed(MouseEvent me)
+        {
+//System.out.println("press");
+            System.out.println("button pressed:"+me.getButton());
+            if(canMousePress)
+            {
+                mousePressed = true;
+                oldDragX = me.getX();
+                oldDragY = me.getY();
+            }
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent me)
+        {
+//System.out.println("release");
+            if(canMousePress)
+                mousePressed = false;
+        }
+    };
+        
+    private MouseMotionAdapter mouseMotionAdapter = new MouseMotionAdapter()
+    {
+        @Override
+        public void mouseDragged(MouseEvent me)
+        {
+            /*if(oldDragX == -99999
+            || oldDragY == -99999)
+            {
+            oldDragX = me.getX();
+            oldDragY = me.getY();
+            return;
+            }*/
+            //System.out.println("dragging");
+            if(mousePressed)
+            {
+                if(firstPaint)
+                {
+                    oldDragX = me.getX();
+                    oldDragY = me.getY();
+                }
+                else
+                {
+            /*System.out.println("oldDragX,Y:"+oldDragX+","+oldDragY);
+            System.out.println("mouseX,Y:"+me.getX()+","+me.getY());
+            System.out.println("placementX,Y:"+placementX+","+placementY);*/
+
+                    int dragDiffX = me.getX() - oldDragX;
+                    int dragDiffY = me.getY() - oldDragY;
+
+                    //System.out.println("dragDiffX,Y:"+dragDiffX+","+dragDiffY);
+
+                    placementX += dragDiffX;
+                    placementY += dragDiffY;
+
+                    oldDragX = me.getX();
+                    oldDragY = me.getY();
+
+                    repaint();
+                }
+            }
+        }
+    };
+
+    public void mouseWheelMoved(MouseWheelEvent mwe)
+    {
+        //parent.stdPrint("MouseWheelEvent coords: "+mwe.getX()+","+mwe.getY());
+        mouseX = mwe.getX();
+        mouseY = mwe.getY();
+
+        //parent.stdPrint("placementX,Y:"+placementX+","+placementY);
+
+        double pixelsLeft = mouseX - placementX;
+        double pixelsAbove = mouseY - placementY;
+
+        //parent.stdPrint("pixelsLeft: "+pixelsLeft);
+        //parent.stdPrint("pixelsAbove: "+pixelsAbove);
+
+        double oldScale = scale;
+
+        double newPixelsAbove;
+        double newPixelsLeft;
+        //update the scale;
+        if(mwe.getWheelRotation() < 0)
+        {//zooming in
+            parent.stdPrint("zooming in");
+            scale *= zoomAmount;
+            repaint();
+            newPixelsAbove = pixelsAbove * zoomAmount;
+            newPixelsLeft = pixelsLeft * zoomAmount;
+        }
+        else
+        {//zooming out
+            parent.stdPrint("zooming out");
+            scale /= zoomAmount;
+            repaint();
+            newPixelsAbove = pixelsAbove / zoomAmount;
+            newPixelsLeft = pixelsLeft / zoomAmount;
+        }
+        //parent.stdPrint("scale/oldScale="+(scale/oldScale));
+        //parent.stdPrint("oldScale/scale="+(oldScale/scale));
+
+        //todo: tidy up bad int and double conversions, avoid double arithmetic where possible
+        //-- to stop the zooming messing up towards the extremes
+        parent.stdPrint("scale change: "+oldScale+" to "+scale);
+
+        //parent.stdPrint("new pixelsLeft: "+newPixelsLeft);
+        //parent.stdPrint("new pixelsAbove: "+newPixelsAbove);
+
+        placementX += (pixelsLeft - newPixelsLeft);//YUSSS I HAVE FUCKING DONE IT AFTER 4 MONTHS
+        placementY += (pixelsAbove - newPixelsAbove);
+
+        parent.stdPrint("new placementX,Y:"+placementX+","+placementY);
+    }
 }
